7588b4d7a1693ecbb0738b6e04982b90ef117b30
nwang <nessie@connect.hku.hk>
2025-03-13 15:17:24 +0100

    udpate 13 mar 2025

---
+++ sketch.js
@@ -0,0 +1,72 @@
+let circles = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    clear();
+    noFill();
+    stroke("#FFF9BD"); // Yellow lines
+
+    let numPatterns = 50; 
+    let emptyZone = width * 0.2; 
+
+    for (let i = 0; i < numPatterns; i++) {
+        let x = random(emptyZone - noise(i) * emptyZone, width);
+        let y = random(height);
+        let radius = random(20, 200);
+        let dotSpacing = random(5, 20);
+
+        circles.push({ x, y, radius, dotSpacing });
+        drawDottedCircle(x, y, radius, dotSpacing);
+    }
+
+    drawRandomGrid(emptyZone);
+}
+
+function drawDottedCircle(x, y, radius, dotSpacing) {
+    let numDots = TWO_PI * radius / dotSpacing;
+    for (let i = 0; i < numDots; i++) {
+        let angle = map(i, 0, numDots, 0, TWO_PI);
+        let xPos = x + cos(angle) * radius;
+        let yPos = y + sin(angle) * radius;
+
+        if (xPos < width * 0.2 && noise(xPos * 0.005, yPos * 0.005) < 0.5) continue;
+
+        strokeWeight(random(1, 3));
+        point(xPos, yPos);
+    }
+}
+
+function drawRandomGrid(emptyZone) {
+    strokeWeight(0.5);
+
+    for (let c of circles) {
+        let gridSize = random(50, 150);
+        let xStart = c.x - c.radius;
+        let yStart = c.y - c.radius;
+        let xEnd = c.x + c.radius;
+        let yEnd = c.y + c.radius;
+
+        for (let x = xStart; x < xEnd; x += gridSize) {
+            if (random() > 0.5) continue;
+            if (x < emptyZone && noise(x * 0.005) < 0.5) continue;
+            line(x, yStart, x, yEnd);
+        }
+
+        for (let y = yStart; y < yEnd; y += gridSize) {
+            if (random() > 0.5) continue;
+            if (y < emptyZone && noise(y * 0.005) < 0.5) continue;
+            line(xStart, y, xEnd, y);
+        }
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    redraw();
+}
+
+function mousePressed() {
+    circles = [];
+    clear();
+    redraw();
+}
